f04d4db9263de45d4cc3c2c1bfb3158efd03ae49
Nahida1318 <bsse1318@iit.du.ac.bd>
2025-06-07 22:13:30 +0600

    Book management system with Django.

---
+++ books/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render,redirect
+from .models import Book
+from .forms import BookForm
+
+# Create your views here.
+
+def homepage(request):
+    books=Book.objects.all()
+    print(books)
+    return render(request,'book_list.html',{'books':books})
+
+
+def add_book(request):
+    if request.method=='POST':
+        form=BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('homepage')
+        
+
+    else:
+        print('get the form')
+        form=BookForm()
+        return render(request,'add_book.html',{'form':form})
+    
+
+
+def delete_book(request,book_id):
+    book=Book.objects.get(id=book_id)
+    if book:
+        book.delete()
+
+    return redirect('homepage')
+
+def update_book(request,book_id):
+    book=Book.objects.get(id=book_id)
+    if request.method=='POST':
+        form=BookForm(request.POST,instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('homepage')
+    else:
+        form=BookForm(instance=book)
+
+    return render(request,'add_book.html',{'form':form})
+
